26ab9fbdd4959a8949b7004038a4416bfe175e97
musmusliu <musmusliu@tencent.com>
2022-10-19 21:04:26 +0800

    fix xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,18 @@
+add_rules("mode.debug", "mode.release")
+
 target("bin2s")
     set_plat("windows")
     set_toolchains("mingw")
     set_kind("binary")
+    add_cxxflags("-std=c++11", "-Wextra", "-Wpedantic", "-Werror")
+    if is_mode("debug") then
+        add_cxxflags("-g")
+    else
+        add_cxxflags("-O3", "-DNDEBUG")
+    end
+    add_files("src/bin2s.cpp")
+    add_includedirs("deps/args")
+
     on_install(function(target)
         if is_subhost("msys") then
             os.cp(target:targetfile() .. ".exe", target:installdir())
@@ -9,12 +20,9 @@ target("bin2s")
             os.cp(target:targetfile(), target:installdir())
         end
     end)
-    add_cxxflags("-std=c++11", "-Wextra", "-Wpedantic", "-Werror")
-    if is_mode("debug") then
-        add_cxxflags("-g")
-    else
-        add_cxxflags("-O3", "-DNDEBUG")
-    end
 
-    add_files("src/bin2s.cpp")
-    add_includedirs("deps/args")
+    on_package(function(target)
+        local packagedir = "$(buildir)/packages/" .. target:name() .. "/"
+        os.cp(target:targetfile(), packagedir)
+    end)
+
